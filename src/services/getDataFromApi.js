f0205da3980c45a2c37c5a64953982212d09169b
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-05-12 12:13:08 +0200

    Create a function to limit the maximum random number

--- src/services/getDataFromApi.js
+++ src/services/getDataFromApi.js
@@ -1,5 +1,6 @@
 const getDataFromApi = () => {
-  return fetch("http://numbersapi.com/random/trivia")
+  const random = Math.floor(Math.random() * 99 + 1);
+  return fetch(`http://numbersapi.com/${random}/trivia`)
     .then((response) => response.text())
     .then((data) => data);
 };
