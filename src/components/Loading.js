f3b82062b1e6bc7c54d2067318554311f9b4f5ae
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-05-16 21:38:56 +0200

    Add loading scss file

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,8 @@
-import "../styleSheets/layout/_noRespond.scss";
+import "../styleSheets/layout/_loading.scss";
 
 const Loading = () => {
   return (
-    <p className="noRespond">
+    <p className="loading">
       Waiting for interesting questions about numbers...
     </p>
   );
