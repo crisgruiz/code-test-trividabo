47d13798498597673abe027137102c4922068d68
Cristina Gonzalez <crisgruiz@protonmail.com>
2021-05-16 16:37:07 +0200

    Add result styles

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,9 +7,9 @@ const Results = (props) => {
         <div key={id} className="answerList">
           <div className="answerList__line"></div>
           <div className="answerList__container">
-            <p>{answer.text}</p>
+            <p className="answerList__question">{answer.text}</p>
             <div className="answerList__answer">
-              <i className="fas fa-check-circle answerList__answer--icon"></i>
+              <i className="fas fa-check-circle answerList__answer--correctIcon"></i>
               <p className="answerList__answer--number">{answer.number}</p>
             </div>
           </div>
@@ -20,11 +20,11 @@ const Results = (props) => {
         <div key={id} className="answerList">
           <div className="answerList__line"></div>
           <div className="answerList__container">
-            <p>{answer.text}</p>
+            <p className="answerList__question">{answer.text}</p>
             <div className="answerList__answer">
-              <i className="fas fa-times-circle answerList__answer--icon"></i>
+              <i className="fas fa-times-circle answerList__answer--wrongIcon"></i>
               <p className="answerList__answer--skip">Skipped</p>
-              <p className="answerList__answer--number">
+              <p className="answerList__answer--text">
                 - Right answer was {answer.number}
               </p>
             </div>
